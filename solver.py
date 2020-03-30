07dd54341c9bdb07c346d35fbe9c9db16fe552c4
Max Nabokow <max.nabokow@gmail.com>
2020-03-30 11:57:42 -0400

    Completed

--- solver.py
+++ solver.py
@@ -10,11 +10,43 @@ board = [
 	[0,4,9,2,0,6,0,0,7]
 ]
 
+def solve(bo):
+	find = findEmpty(bo)
+
+	if not find:
+		return True
+	else:
+		row,col = find
+	
+	for i in range(1, 10):
+		if valid(bo, i, (row, col)):
+			bo[row][col] = i
+			
+			if solve(bo):
+				return True
+			bo[row][col] = 0
+	return False
+
 def valid(bo, num, pos):
 	# Check row
 	for i in range(len(bo[0])):
 		if bo[pos[0]][i] == num and pos[1] != i:
 			return False	
+
+	# Check column
+	for i in range(len(bo)):
+		if bo[i][pos[1]]  == num and pos[0] != i:
+			return False	
+
+	# Check 3x3 box
+	boxX = pos[1] // 3
+	boxY = pos[0] // 3
+
+	for i in range(boxY * 3, boxY * 3 + 3):	
+		for j in range(boxX * 3, boxX * 3 + 3):	
+			if bo[i][j] == num and (i,j) != pos:
+				return False
+	return True
 
 def printBoard(bo):
 	for i in range(len(bo)):	
@@ -32,6 +64,12 @@ def printBoard(bo):
 
 def findEmpty(bo):
 	for i in range(len(bo)):
-		for j in range(len(bo[9])):
+		for j in range(len(bo[0])):
 			if bo[i][j] == 0:
 				return (i,j) # row, col
+	return None
+
+printBoard(board)
+solve(board)
+print()
+printBoard(board)
